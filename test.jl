79ce0caefb0e2d94ae760e7296251195eaba3079
Unix&Me <unixnme@gmail.com>
2021-08-11 12:03:53 -0700

    test with apple

--- test.jl
+++ test.jl
@@ -1,7 +1,8 @@
 using AdaGram
 vm, dict = load_model("text8.bin")
 
-println(expected_pi(vm, dict.word2id["bank"]))
-println(nearest_neighbors(vm, dict, "bank", 1, 10))
-println(disambiguate(vm, dict, "bank", split("river")))
-println(disambiguate(vm, dict, "bank", split("money")))
+println(expected_pi(vm, dict.word2id["apple"]))
+println(nearest_neighbors(vm, dict, "apple", 1, 10))
+println(nearest_neighbors(vm, dict, "apple", 2, 10))
+println(disambiguate(vm, dict, "apple", split("computer")))
+println(disambiguate(vm, dict, "apple", split("fruit")))
