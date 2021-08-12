11dcb25481405be91d389472437f3cbcd03e245f
Unix&Me <unixnme@gmail.com>
2021-08-11 19:44:31 -0700

    works better with new

--- test.jl
+++ test.jl
@@ -1,8 +1,8 @@
 using AdaGram
 vm, dict = load_model("text8.bin")
 
-println(expected_pi(vm, dict.word2id["apple"]))
-println(nearest_neighbors(vm, dict, "apple", 1, 10))
-println(nearest_neighbors(vm, dict, "apple", 2, 10))
-println(disambiguate(vm, dict, "apple", split("computer")))
-println(disambiguate(vm, dict, "apple", split("fruit")))
+println(expected_pi(vm, dict.word2id["new"]))
+println(nearest_neighbors(vm, dict, "new", 1, 10))
+println(nearest_neighbors(vm, dict, "new", 2, 10))
+println(disambiguate(vm, dict, "new", split("york")))
+println(disambiguate(vm, dict, "new", split("create")))
